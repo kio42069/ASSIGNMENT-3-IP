5136639eec2c6967692b6ab304dfba80f7242297
Surat Sathi Samanta <62372847+surat-ss@users.noreply.github.com>
2023-01-22 10:24:03 +0530

    q1 completed and optimised to just 9 lines YUH

---
+++ q1.py
@@ -0,0 +1,9 @@
+n = int(input())
+def printer(n,t):
+    print("*"*t+" "*(n-t)+" "*(n-t)+"*"*t)
+    printer(n,t-1) if t != 1 else print("",end = "")
+def printout(n,t):
+    print("*"*t+" "*(n-t)+" "*(n-t)+"*"*t)
+    printout(n,t+1) if t!= n else print("",end = "")
+printer(n,n)
+printout(n,1)
