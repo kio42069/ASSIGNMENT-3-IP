4c698d10c9ce3e92b8cd15c170dbd2c00b50eb98
Surat Sathi Samanta <62372847+surat-ss@users.noreply.github.com>
2023-02-08 22:01:54 +0530

    shitr

--- q2.py
+++ q2.py
@@ -2,4 +2,8 @@ with open("records.txt", "r") as file:
     records = file.readlines()
 processed_records = []
 for i in records:
-    processed_records.append(i.split())
+    processed_records.append(i.split(", "))
+    
+for i in range(len(processed_records)):
+    processed_records[i][-1] = int(processed_records[i][-1][0:2])*60*60+int(processed_records[i][-1][3:5])*60+int(processed_records[i][-1][6:8])
+    
